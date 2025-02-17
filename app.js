e4ec67ba68fd433fb858eb9dea5e6f352a3fbe89
YDK Back <forgeryor878@gmail.com>
2025-02-17 15:34:25 +0800

    Design changes

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(adminRoutes); 
+
+app.use(express.static("style"));
 
 app.listen(3000, '0.0.0.0', () => console.log("Server running on port 3000"));
